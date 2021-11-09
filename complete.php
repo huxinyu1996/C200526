fa91ddc1568b66c678676491048c76d8c6078cf5
huxinyu1996 <84057422+huxinyu1996@users.noreply.github.com>
2021-11-09 14:51:53 +0900

    Update complete.php

--- complete.php
+++ complete.php
@@ -1,9 +1,35 @@
 <?php
   session_start();
+  require_once("functions.php"); 
 
   $name = $_SESSION['name'];
-  $hobby = $_SESSION['email'];
+  $email = $_SESSION['email'];
   $gender = $_SESSION['gender'];
+
+$dbh = db_conn();  
+try{ 
+
+      $sql = "INSERT INTO user (email, name, gender) VALUE (:email, :name, :gender)"; 
+
+      $stmt = $dbh->prepare($sql);                           //クエリの実行準備 
+
+      $stmt->bindValue(':email', $email, PDO::PARAM_STR);      //バインド:プレースホルダ―を埋める 
+
+      $stmt->bindValue(':name', $name, PDO::PARAM_STR);  //バインド:プレースホルダ―を埋める 
+
+      $stmt->bindValue(':gender', $gender, PDO::PARAM_INT); //バインド:プレースホルダーを埋める 
+
+      $stmt->execute();                                        //クエリの実行 
+
+      $dbh = null;                                             //MySQL接続解除 
+
+}catch (PDOException $e){ 
+
+      echo($e->getMessage()); 
+
+    die(); 
+
+} 
 ?>
 
 <!DOCTYPE html>
